a71d0df93791022953b4da9ab1e0d42cec966a07
DomH <dominique.hathi@wanadoo.fr>
2018-01-22 22:09:57 +0100

    dhi pagination ok

--- pages/code_user_requete.php
+++ pages/code_user_requete.php
@@ -1,33 +1,7 @@
 <?php
 //require('session_start.php');
-/*echo '$_SESSION["page"]'.$_SESSION['page'];
-echo '---------';
-echo '<br/>';
-echo '$_SESSION[\'email\']'.$_SESSION['email'];
-echo '---------';
-echo '<br/>';
-echo '$_SESSION[\'password\']'.$_SESSION['password'];
-echo '---------';
-echo '<br/>';
-echo '$_SESSION[\'qualification\']'.$_SESSION['qualification'];
-echo '---------';
-echo '<br/>';
-echo '$_SESSION["data_to_modify"]';
-print_r($_SESSION['data_to_modify']);
-echo '---------';
-echo '<br/>';
-echo '$_POST["requete"]'.$_POST['requete'];
-echo '---------';
-echo '<br/>';
-echo '$_SESSION["requete"]'.$_SESSION['requete'];
-echo '---------';
-echo '<br/>';
 
-echo 'DB_HOST :'.DB_HOST;
-echo 'DB_NAME :'.DB_NAME;
-*/
-
-/* ouverture de la bd */
+/* OUVERTURE DE LA BD */
 try {
     $bdd = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', $_SESSION["email"], $_SESSION["password"]);
 } catch (Exception $e) {
@@ -40,7 +14,7 @@ if (isset($_POST['requete'])) {
 }
 
 
-/* teste si c'est une requete avec un input */
+/* TEST SI REQUETE AVEC INPUT (cas de la requete 7) */
 if (empty($_POST['requete_input'])) {
     $nbreq = $_POST['requete'];
     $req = $bdd->query("SELECT requetesql FROM requete WHERE numero = " . $nbreq . ";");
@@ -57,43 +31,26 @@ if (empty($_POST['requete_input'])) {
     $tablereq = $tablereq->fetchAll(PDO::FETCH_ASSOC);
 }
 
-/*echo '$nbreq :'.$nbreq;
-echo '$req : ';
-echo '<pre>';
-print_r($req);
-echo '</pre>';
-echo "<br/>";
-echo "-------";
-echo '$nom_requete : ';
-echo '<pre>';
-print_r($nomreq);
-echo '</pre>';
-echo "<br/>";
-echo "-------";
-echo '$tablereq : ';
-echo '<pre>';
-print_r($tablereq[0]['table_request']);
-echo '</pre>';
-echo "<br/>";
-echo "-------";
-die();*/
 
+// avant yavait le decompte de page et autre
 /* ligne ajoutée pour la pagination */
+
+
 //Nous allons afficher 5 messages par page.
-$dataParPage = 5; 
+//$dataParPage = 5; 
 //Nous récupérons le contenu de la requête dans $retour_total
-$retour_total = $bdd->query("SELECT COUNT(*) AS total FROM " . $tablereq[0]['table_request'] . ";");
+//$retour_total = $bdd->query("SELECT COUNT(*) AS total FROM " . $tablereq[0]['table_request'] . ";");
 //On range retour sous la forme d'un tableau. 
-$donnees_total = $retour_total->fetchAll(PDO::FETCH_ASSOC);
+//$donnees_total = $retour_total->fetchAll(PDO::FETCH_ASSOC);
 //On récupère le total pour le placer dans la variable $total.
-$total = $donnees_total[0]['total'];
-echo $total;
+//$total = $donnees_total[0]['total'];
+//echo $total;
 //Nous allons maintenant compter le nombre de pages.
-$nombreDePages = ceil($total/$dataParPage);
-echo '$nombreDePages :'.$nombreDePages;
+//$nombreDePages = ceil($total/$dataParPage);
+//echo '$nombreDePages :'.$nombreDePages;
 
 // vérif de la page
-if(isset($_GET['page'])) // Si la variable $_GET['page'] existe...
+/*if(isset($_GET['page'])) // Si la variable $_GET['page'] existe...
 {
      $pageActuelle = $_GET['page'];
 
@@ -108,94 +65,134 @@ else // Sinon
      $pageActuelle = 1; // La page actuelle est la n°1
     $_SESSION['page'] = $pageActuelle;
 }
-/*echo '$pageActuelle :'.$pageActuelle;*/
+/* On calcul la première entrée à lire pour le LIMIT*/
+/*$premiereEntree = ($pageActuelle-1)*$dataParPage; */
 
 
-$premiereEntree = ($pageActuelle-1)*$dataParPage; // On calcul la première entrée à lire
-//echo '$premiereEntree :'.$premiereEntree;
 
-/* verify if input (request 7) with a request */
+
+
+/* VERIFY SI UN INPUT EXISTE EN PLUS DE LA REQUETE (request 7) */
 if (empty($_POST['requete_input'])) {
     $requete = $req->fetchAll(PDO::FETCH_ASSOC);
+    
+    // calcul du nombre total max à afficher
+    $toto = $bdd->query($requete[0]['requetesql']);
+    $tutu = $toto->fetchAll(PDO::FETCH_ASSOC);
+    $tutu2 = $tutu;
+    $tutu_total = count($requete[0]['requetesql']);
+    $total = count($tutu2);
+
+        //Nous allons afficher 5 messages par page.
+        $dataParPage = 5; 
+       
+        //Nous allons maintenant compter le nombre de pages.
+        $nombreDePages = ceil($total/$dataParPage);
+        //echo '$nombreDePages :'.$nombreDePages;
+
+        // vérif de la page
+        if(isset($_GET['page'])) // Si la variable $_GET['page'] existe...
+        {
+             $pageActuelle = $_GET['page'];
+
+             if($pageActuelle > $nombreDePages) // Si la valeur de $pageActuelle (le numéro de la page) est plus grande que $nombreDePages...
+             {
+                  $pageActuelle = $nombreDePages;
+                  $_SESSION['page'] = $pageActuelle;
+             }
+        }
+        else // Sinon
+        {
+             $pageActuelle = 1; // La page actuelle est la n°1
+            $_SESSION['page'] = $pageActuelle;
+        }
+        /* On calcul la première entrée à lire pour le LIMIT*/
+        $premiereEntree = ($pageActuelle-1)*$dataParPage; 
+
+
+    
     $nom_requete = $nomreq->fetchAll(PDO::FETCH_ASSOC);
         $donnees_total = $requete;
         $donnees_total = substr($donnees_total[0]['requetesql'], 0,-1);
         $donnees_total = $donnees_total.' LIMIT '.$premiereEntree.', '.$dataParPage;
         $donnees_total = array('0' => array('requetesql' => $donnees_total));
     $requete = $donnees_total;
-    print_r($requete);
+    /*print_r($requete);
+    echo '------';*/
+    //print_r($toto);
 } else {
     $requete = $req->fetchAll(PDO::FETCH_ASSOC);
+
+    // Add calcul de la totalité
+    // calcul du nombre total max à afficher
+    $toto = $bdd->query($requete[0]['requetesql']);
+    $tutu = $toto->fetchAll(PDO::FETCH_ASSOC);
+    $tutu2 = $tutu;
+    $tutu_total = count($requete[0]['requetesql']);
+    $total = count($tutu2);
+
+    //Nous allons afficher 5 messages par page.
+        $dataParPage = 5; 
+       
+        //Nous allons maintenant compter le nombre de pages.
+        $nombreDePages = ceil($total/$dataParPage);
+        //echo '$nombreDePages :'.$nombreDePages;
+
+        // vérif de la page
+        if(isset($_GET['page'])) // Si la variable $_GET['page'] existe...
+        {
+             $pageActuelle = $_GET['page'];
+
+             if($pageActuelle > $nombreDePages) // Si la valeur de $pageActuelle (le numéro de la page) est plus grande que $nombreDePages...
+             {
+                  $pageActuelle = $nombreDePages;
+                  $_SESSION['page'] = $pageActuelle;
+             }
+        }
+        else // Sinon
+        {
+             $pageActuelle = 1; // La page actuelle est la n°1
+            $_SESSION['page'] = $pageActuelle;
+        }
+        /* On calcul la première entrée à lire pour le LIMIT*/
+        $premiereEntree = ($pageActuelle-1)*$dataParPage; 
 
     foreach ($requete[0] as $key => $value) {
         $reponse = $bdd->query($value);
         $requete = str_replace('$requete_input', $requete_input, $value);
         $requete = array('0' => Array('requetesql' => $requete));
             $donnees_total = $requete;
-                /*echo '$donnees_total : ';
-                echo '<pre>';
-                print_r($donnees_total);
-                echo '</pre>';
-                echo "<br/>";
-                echo "-------";*/
             $nb_donnees_total = strlen($donnees_total[0]['requetesql']);
-                /*echo $nb_donnees_total;*/
             $donnees_total = substr($donnees_total[0]['requetesql'], 0, $nb_donnees_total);
-                /*echo '$donnees_total : ';
-                echo '<pre>';
-                print_r($donnees_total);
-                echo '</pre>';
-                echo "<br/>";
-                echo "-------";*/
             $donnees_total = $donnees_total.' LIMIT '.$premiereEntree.', '.$dataParPage;
             $donnees_total = array('0' => array('requetesql' => $donnees_total));
         $requete = $donnees_total;
     }
     $nom_requete = $nomreq->fetchAll(PDO::FETCH_ASSOC);
 }
-/* verif si la requete est correcte */
 
-/*echo '$requete : ';
-echo '<pre>';
-print_r($requete);
-echo '</pre>';
-echo "<br/>";
-echo "-------";
-echo '$nom_requete : ';
-echo '<pre>';
-print_r($nom_requete);
-echo '</pre>';
-echo "<br/>";
-echo "-------";
-die();*/
 
 /* display the title (one of 8 request) */
 foreach ($nom_requete[0] as $key => $value) {
     $nom_requetetitre = $value;
 }
-echo '<p>voici le résultat pour : ' . $nom_requetetitre . '</p>';
+echo '<p>voici le résultat pour : <b>' . $nom_requetetitre . '</b></p>';
 
-/* la vrai requete  pour les datas du tableau */
+/* LA VRAI REQUETE POUR LES DATAS DU TABLEAU */
 foreach ($requete[0] as $key => $value) {
     $reponsetableau = $bdd->query($value);
-    /*echo '$reponsetableau : ';
-    echo '<pre>';
-    print_r($reponsetableau);
-    echo '</pre>';
-    echo "<br/>";
-    echo "-------";
-    die();*/
+   
 }
 $line = $reponsetableau->fetchAll(PDO::FETCH_ASSOC);
 
-/* affichage du head du tableau */
+/* -------- AFFICHAGE Du HEAD DU TABLEAU --------------------- */
 echo '<table class="striped responsive-table">
 	    		<thead>';
 foreach ($line[0] as $key => $value) {
     echo '<th>' . $key . '</th>';
 }
 echo '</thead>';
-// display data of the tableau
+// DISPLAY DATA OF THE TABLEAU
 echo '<tbody>';
 foreach ($line as $key => $valArray) {
     echo '<tr>';
@@ -205,18 +202,16 @@ foreach ($line as $key => $valArray) {
     echo '</tr>';
 }
 echo '</tbody>';
-/*echo '<div></div>';*/
 echo '</table>';
 
 $_SESSION['requete'] = $_POST['requete'];
 
-/* affiche de la pagination */
+/* -------- AFFICHAGE DE LA PAGINATION --------------------- */
 echo '</p>';
 
 // si une seule page à afficher pas de pagination
-echo $nombreDePages;
+//echo $nombreDePages;
 if ($nombreDePages <> 1) {
-    //echo '<p align="center">Page : '; //Pour l'affichage, on centre la liste des pages
     echo '<ul class="pagination">
         <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>';
 
@@ -229,10 +224,10 @@ if ($nombreDePages <> 1) {
     }
     echo '<li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>';
     echo '</ul>';
-}else {
-
-
-    echo '<li class="active"><a href="page_resultat_utilisateur.php?page=".$i.</a>'.$i.'</li>';
+} else {
+    echo '';
+    echo '<br/>';
+    //echo '<li class="active"><a href="page_resultat_utilisateur.php?page=".$i.</a>'.$i.'</li>';
 }
 
 $reponsetableau->closeCursor();
